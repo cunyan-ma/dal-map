11f39d03bf12843f42126c3c74c7426768de08b8
cunyan-ma <cunyan_ma@brown.edu>
2026-06-18 22:48:08 -0400

    about + method populated some

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -9,19 +9,71 @@ function AboutPage() {
 
     return (
         <div className="about-container">
-            <h1>About this project</h1>
+            <h1>About this project (very rough draft)</h1>
             <p>
-                This map visualizes the geographic footprint of the global data
-                labeling industry, focusing on where companies maintain teams
-                rather than where they are headquartered.
+                This map tracks the geographic locations of data workers
+                who annotate data to render them legible to machine with little
+                pay or recognition. It provides the first systematic and
+                quantitative documentation of workers' geographic region,
+                visualized via an interactive global map that ties workers
+                around the globe with contracting platforms and custmers
+                concentrated in the Western world.
+            </p>
+            <br></br>
+            <p>
+                This map is part of the data journalism series: where is the
+                material supply chain behind AI? *insert hyperlink here after finished*
             </p>
 
+            <h2>Background</h2>
+            <p>
+                Thanks to some incredible journalists, the story of data workers
+                have surfaced to us more as AI became a larger part of our
+                lives. We know that some of them locate in Kenya, Venezuela,
+                and other countries with English literacy but poor economic
+                conditions which enable exploitation. We know that they are
+                subjected to gruesome work, often made to work with traumatizing
+                material with no redress from their supposed employees--the
+                "data labeling platforms" that pass down contracts from
+                mysterious customers.
+
+            </p>
+            <br></br>
+            <p>
+                More than anything, we know that the Data Annotation and
+                Labeling (DAL) industry is an incredibly opaque industry by
+                design. Half of the industry operates as gig platforms, which
+                treats workers as "contractors" rather than "workers", obscuring
+                away any detail on worker number, location, wage, etc to the
+                public.
+            </p>
+
+            <br></br>
+            <p>
+                The other half of the industry, however, operates as "Business
+                Process Outsourcing" (BPO) companies. These companies receive
+                data labeling contracts outsourced from AI companies, then pass
+                them along to their in-house annotation teams assembled across
+                "delivery centers" across the world. The locations of these
+                delivery centers are chosen deliberately. Not only do the
+                companies need to guarantee company network, these locations
+                also often have abundant of workers who look for lower pay due
+                to their countries' historic, social, or economic conditions.
+            </p>
+
+            <br></br>
+            <p>
+                This map tracks the worker location for the second type of
+                companies.
+            </p>
+
+            <br></br>
+
             <h2>Methodology</h2>
-            <ul>
-                {methodology.map(item => (
-                    <li key={item}>{item}</li>
-                ))}
-            </ul>
+
+            <p>
+
+            </p>
 
             <h2>Data source</h2>
             <p>
--- src/pages/Methodology.jsx
+++ src/pages/Methodology.jsx
@@ -9,14 +9,90 @@ function Methodology() {
 
     return (
         <div className="method-container">
-            <h1>Methodology</h1>
-            <p>{introduction}</p>
+            <h1>Methodology (very rough draft)</h1>
+            <p>
+                The data behind the map is collected via the following steps: 
+                locate data labeling platforms, locate their worker delivery 
+                centers, locate their custoemrs, then visualizing them via a 
+                React app on leaflet library.
+            </p>
+
+            <br>
+            </br>
+
+            <p>
+                It's important to highlight that this map is not a comprehensive 
+                database of ALL BPO data labeling companies and ALL of their 
+                delivery centers. The magnitude of search is incredible, 
+                therefore this is a live project that will need continual 
+                updating. The author is less concerned with capturing the 
+                entirety of the data labeling companies, but more to display a 
+                consistent pattern across the industry--where are the workers 
+                located across the world, and what type of customers do they 
+                serve?
+            </p>
 
             <h2>How were companies located</h2>
-            <p>{companies}</p>
+            <p>
+                Lots of existing research done on this. TechEquity's Data Work 
+                Landscape is a good starting point. Lots of industry reports. 
+                All of these resources are combined together to get the most 
+                comprehensive list of data labeling companies that exist in the 
+                English media world. LLM is used to assist with cross-referncing 
+                between existing resources.
+            </p>
+
+            <br>
+            </br>
+
+            <p>
+                After a list of companies is collected, we do desk research on 
+                each company to determine their business model. TechEquity's 
+                database already includes some of those information. The BPO 
+                ones are preserved for the scope of this project. Their HQ info 
+                is pulled from web as publicly avaialble info.
+            </p>
+
+            <br>
+            </br>
+
+            <p>
+                Not all BPO companies are immediately included on the map. Its 
+                delivery centers have to be discoverable. More on that in the 
+                next section.
+            </p>
 
             <h2>How were delivery centers located located</h2>
-            <p>{delivery}</p>
+            <p>
+                In most cases, delivery center = regional offices.
+
+            </p>
+
+            <br>
+            </br>
+
+            <p>
+                For publicly traded companies--which makes up an incredibly 
+                small fraction of the ecosystem--this information is available 
+                via their SEC 10-K Exhibit 21, subsidiary information. For 
+                private companies, this can be searched either from their 
+                LinkedIn or website. Not all private companies release this 
+                information. Specificity of address is not guaranteed, 
+                entirely based on how much info the company can provide.
+            </p>
+
+            <br>
+            </br>
+
+            <p>
+                For this step, the author used LLM to filter out companies with 
+                little disclosed information by asking it to try locate their 
+                regional offices. The author will not yet spend time to look 
+                into companies that even LLM cannot find information for. For 
+                any company that LLM tags with available informatino, the author 
+                will conduct desk search until locating an exact source. 
+                Otherwise the company is not included in the map. 
+                All comapny + deliver center + source can be found via "database".             </p>
 
             <h2>Locating relatinoships between customers and companies</h2>
             <p>{customer}</p>
